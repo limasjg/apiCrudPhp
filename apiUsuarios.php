3feeebbd9b4d72489a599ee5dbff3e87c7ef62fe
limasjg <limasjg@outlook.com>
2020-08-17 17:00:06 -0300

    acerto

--- apiUsuarios.php
+++ apiUsuarios.php
@@ -8,16 +8,16 @@ $postjson = json_decode(file_get_contents('php://input'), true);
 //LISTAGEM DOS USUARIOS E PESQUISA PELO NOME E EMAIL
 
 if($postjson['requisicao'] == 'listar'){
-    
-    $query = mysqli_query($mysqli, "select * from usuarios order by id desc
-     limit $postjson[start], $postjson[limit]");
-    }
+
+
+  $query = mysqli_query($mysqli, "select * from usuarios order by id desc limit $postjson[start], $postjson[limit]");
+
 
  	while($row = mysqli_fetch_array($query)){ 
  		$dados[] = array(
  			'id' => $row['id'], 
  			'nome' => $row['nome'],
-			'email' => $row['email'],
+			'usuario' => $row['usuario'],
       'senha' => $row['senha'],
             
         
@@ -35,3 +35,21 @@ if($postjson['requisicao'] == 'listar'){
             echo $result;
 
 
+}else if($postjson['requisicao'] == 'add'){
+
+    $query = mysqli_query($mysqli, "INSERT INTO usuarios SET nome = '$postjson[nome]', usuario = '$postjson[usuario]', senha = '$postjson[senha]' ");
+
+    $id = mysqli_insert_id($mysqli);
+     
+
+    if($query){
+    $result = json_encode(array('success'=>true, 'id'=>$id));
+
+  }else{
+    $result = json_encode(array('success'=>false));
+
+  }
+   echo $result;
+  
+
+}
